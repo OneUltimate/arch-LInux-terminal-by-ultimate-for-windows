0bcb8df5ecbf89659459b9d3b678d8fc745feb5c
oneultimate <168085923+OneUltimate@users.noreply.github.com>
2025-07-31 17:04:57 +0300

    Update help_text.py

--- help_text.py
+++ help_text.py
@@ -1,13 +1,20 @@
 
 
-HELP_TEXT = """
+HELP_TEXT_1 = """
 Available commands:
 - cd [directory]  : Change directory
 - clear           : Clear the terminal 
 - exit/quit       : Exit the application
 - help            : Show this help message
 - restart         : Run DSI >>> (/'help dsi')
-- start&stopbot   : start/stop tg bot
-
 Any other command will be executed in the system shell
 """
+
+
+HELP_TEXT_2 = """
+Available commands:
+- start           : start bot tg
+- stopbot         : stop bot tg
+"""
+
+HELP_TEXT_3 = """_3_"""
