5f59416d6375bc75044d6db5a6d0b3d6f03c3548
oneultimate <168085923+OneUltimate@users.noreply.github.com>
2025-08-10 16:24:56 +0300

    Update weather.py

--- modules_plus/weather.py
+++ modules_plus/weather.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 CITY = os.getenv("CITY")
 
-city1 = CITY or ['Москва', 'Рязань', 'Уфа', 'Париж']
+city = CITY or ['Москва', 'Рязань', 'Уфа', 'Париж']
 
 for city1 in city1:
-    url1 = 'https://api.openweathermap.org/data/2.5/weather?q='+city1+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+    url1 = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
 
 def temp1():
     try:
